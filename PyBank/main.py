7f170b69210e28e84c26d893250b7da7aec4ccb5
Skyler Khalachyan <skyler.b.dixon@gmail.com>
2023-03-26 22:50:18 -0700

    Work in progress

---
+++ PyBank/main.py
@@ -0,0 +1,19 @@
+import csv
+
+csvpath ="../PyPoll/Resources/election_data.csv"
+
+##UNCOMMENT THIS_ with open(csvpath,'r') as csvfile: 
+
+
+
+# Get the total number of months included in the dataset
+
+#The net total amount of "Profit/Losses" over the entire period
+
+#The changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+#The greatest increase in profits(date and amount) over the entire period
+
+#The greatest decrease in profits (date and amount) over the entire period
+
+
