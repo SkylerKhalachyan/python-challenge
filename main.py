a1bfb59c7fc846502eea34a5ceaa999820f54b06
Skyler Khalachyan <skyler.b.dixon@gmail.com>
2023-04-11 14:19:09 -0700

    Practicing pushing saves

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    #print(csvreader)
 
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
